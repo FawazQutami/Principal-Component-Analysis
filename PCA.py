e55de9b7c5d79c2ece5cd918d1c34049c66e213c
apriLLion <28001843+FawazQutami@users.noreply.github.com>
2021-03-29 06:32:22 +0300

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn import datasets
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import StandardScaler
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -22,12 +22,8 @@ class PCA:
      only the first few principal components to obtain lower-dimensional data while preserving as 
      much of the data's variation as possible.
      
-     It can be shown that the principal components are eigenvectors of the data's covariance matrix.
-    """
-
-    """ 
-    The principal components are eigenvectors of the data's covariance matrix.
-    Thus, the principal components are often computed by:
+     The principal components are eigenvectors of the data's covariance matrix.
+     Thus, the principal components are often computed by:
             - EVD: Eigenvalue decomposition of the data covariance matrix or 
             - SVD: Singular Value Decomposition of the data matrix.
     """
@@ -36,31 +32,31 @@ class PCA:
         self.n_components = n_components
         self.Feature_vector = None
         self.eigenvalues = None
-        self.sta = None
+        #self.mean = None
+        #self.sta = None
 
     def fit_transform(self, X):
-        # STEP 1: STANDARDIZATION:
+        # STEP 1: STANDARDIZATION OF FEATURE DATA:
         scaled_features = scaling_data(X)  # X = (X - self.mean)/self.sta
 
-        # STEP 2: COVARIANCE MATRIX COMPUTATION
-        # Compute the covariance matrix of the data
-        """
-             Cov(X, X) = 1/N ∑i=1:n (xi - X_bar)((xi - X_bar).T)  
+        # STEP 2: COMPUTE THE COVARIANCE MATRIX
+        """        
+            Cov(X, X) = 1/N ∑i=1:n (xi - X_bar)((xi - X_bar).T)  
                      
-        Covariance provides the a measure of strength of correlation between two variable or more set of variables. 
+            Covariance provides the a measure of strength of correlation between two variable 
+            or more set of variables. 
                 If COV = 0 then variables are uncorrelated
                 If COV > 0 then variables positively correlated
                 If COV > < 0 then variables negatively correlated
-        """
-        """
-        np.cov() function: estimate a covariance matrix, given data and weights.
-        Each row of the matrix represents a variable, and each column a single observation of all those variables
-        But, X is (n_samples, n_features) matrix that need to be transposed to X.T (n_features, n_samples)
-        """
+        
+        # np.cov() function: estimate a covariance matrix, given data and weights.
+            Each row of the matrix represents a variable, and each column a single observation of 
+            all those variables but, X is (n_samples, n_features) matrix that need to be transposed 
+            to X.T (n_features, n_samples)"""
         transposed_features = scaled_features.T
         cov_matrix = np.cov(transposed_features)
 
-        # STEP 3 Eigendecomposition: COMPUTE THE EIGENVECTORS AND EIGENVALUES OF THE COVARIANCE MATRIX TO IDENTIFY THE
+        # STEP 3 Eigen-decomposition: COMPUTE THE EIGENVECTORS AND EIGENVALUES OF THE COVARIANCE MATRIX TO IDENTIFY THE
         # PRINCIPAL COMPONENTS
         """
             Eigendecomposition: is a process that decomposes a square matrix into eigenvectors and eigenvalues. 
@@ -69,38 +65,40 @@ class PCA:
          
             The eigenvectors points in the same direction of the maximum variation between 2 variables,
             and the associated eigenvalues indicates the importance of it's eigenvectors
-            """
-        # linalg.eig(a): Compute the eigenvalues and right eigenvectors of a square array.
-        self.eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
-        """
+            
             The normalized (unit “length”) eigenvectors, such that the column v[:,i] is the eigenvector 
             corresponding to the eigenvalue w[i].
-            """
-        # Transposed the vector column(eigenvectors) for easier transformation
-        eigenvectors_transposed = eigenvectors.T
-        # Sort eigenvalues in descendant order
+        
+        # linalg.eig(A): Compute the eigenvalues and right eigenvectors of A square array."""
+        self.eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
         """ 
-            Observe that the big elements are coming last in the argsort. 
-            So, you can read from the tail of the argsort 
-            """
+        # Transposed the vector column(eigenvectors) for easier transformation"""
+        eigenvectors_transposed = eigenvectors.T
+        """  
+        # Sort eigenvalues in descendant order:
+           Observe that the big elements are coming last in the argsort. 
+           So, you can read from the tail of the argsort """
         indices = np.argsort(self.eigenvalues)[::-1]
-        # Choose eigenvectors according to the new descendant-order eigenvalues
+        """
+        # Choose eigenvectors according to the new descendant-order eigenvalues"""
         # new_eigenvalues = self.eigenvalues[indices]
         new_eigenvectors = eigenvectors_transposed[indices]
 
-        # STEP 4: FEATURE VECTOR
-        # Choose whether to keep all these components or discard those of lesser significance
-        # (of low eigenvalues), and form with the remaining ones a matrix of vectors that
-        # we call Feature vector.
-        # This is the first step towards dimensionality reduction, because if we choose to keep
-        # only p eigenvectors (components) out of n, the final data set will have only p dimensions.
+        # STEP 4: CHOOSE THE FEATURE VECTOR
+        """
+            Choose whether to keep all these components or discard those of lesser significance
+            (of low eigenvalues), and form with the remaining ones a matrix of vectors that
+            we call Feature vector.
+            This is the first step towards dimensionality reduction, because if we choose to keep
+            only p eigenvectors (components) out of n, the final data set will have only p dimensions.
 
-        # Store only p eigenvectors as new components
+        # Store only p eigenvectors as new components"""
         self.Feature_vector = new_eigenvectors[0:self.n_components]
 
-        # FINALLY: RECAST THE DATA ALONG THE PRINCIPAL COMPONENTS AXES
-        # Project the data according to the new dimension - note that we have to transpose the Feature_vector
-        # as we already transposed the vector column of eigenvectors
+        # FINALLY: PROJECT THE DATA ALONG THE PRINCIPAL COMPONENTS AXES
+        """
+            Project the data according to the new dimension - note that we have to transpose the Feature_vector
+            as we already transposed the vector column of eigenvectors"""
         projected_vectors = np.dot(scaled_features, self.Feature_vector.T)
 
         return projected_vectors
@@ -113,7 +111,6 @@ def plot_pca(pca_x, y):
     fig = plt.figure()
     plt.scatter(x1, x2,
                 c=y
-                , edgecolor='r'
                 , marker='o'
                 , s=50
                 , cmap=plt.cm.get_cmap('viridis', 3)
@@ -133,7 +130,7 @@ def explained_variance(e_values):
     # The first value is just the sum of explained variances — and must be equal to 1
     print('\n-- The sum of explained variances — and must be equal to 1:\n\t', np.sum(e_variances))
     # The second value is an array, representing the explained variance percentage per principal component.
-    ev = [format(x * 100 , '.3f') for x in e_variances]
+    ev = [format(x * 100, '.3f') for x in e_variances]
     print('-- Explained variance percentage per principal component:\n\t', ev)
 
 
@@ -193,9 +190,7 @@ if __name__ == '__main__':
     res['flower'] = target.apply(lambda x: iris.target_names[x])
     print(res.head())
 
-    import matplotlib.pyplot as plt
     import seaborn as sns
-
     plt.figure(figsize=(8, 6))
     sns.scatterplot(res['PC1'], [0] * len(res), hue=res['flower'], s=200)
     plt.show()
